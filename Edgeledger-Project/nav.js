e52664e806df29f7ecf9a217ac48d6913e09bb1d
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-02-19 15:24:53 +0600

    add js for nav

--- Edgeledger-Project/nav.js
+++ Edgeledger-Project/nav.js
@@ -3,10 +3,10 @@ const navbar = document.getElementById("nav-edge");
 let scrolled = false;
 
 window.onscroll = function () {
-  if (window.pageYOffset > 10) {
+  if (window.pageYOffset > 100) {
     navbar.classList.remove("top");
     if (!scrolled) {
-      navbar.style.transform = "translateY(-90px)";
+      navbar.style.transform = "translateY(-70px)";
     }
     setTimeout(function () {
       navbar.style.transform = "translateY(0)";
